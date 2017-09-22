53ef6a057191b237965da552738ddb2ac2346c1b
Kevin McGillivray <kevin.mcgillivray@me.com>
2017-09-22 16:00:59 -0500

    Remove extra menu

--- modules/menus.php
+++ modules/menus.php
@@ -3,8 +3,7 @@
 function register_menus() {
 	register_nav_menus(
 		array(
-		 'main-menu' => __( 'Main Menu' ),
-		 'footer-menu' => __( 'Footer Menu' )
+		 'main-menu' => __( 'Main Menu' )
 		)
 	);
 }
