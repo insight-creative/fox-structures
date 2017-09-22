ad9f7a2c22c9d9e6cb3aa11b4627c54d75ffd77c
Kevin McGillivray <kevin.mcgillivray@me.com>
2017-09-22 11:12:28 -0500

    Fix category links

--- includes/home-sections/categories.php
+++ includes/home-sections/categories.php
@@ -2,19 +2,19 @@
   <nav class="sub-nav">
     <ul class="nav nav-pills nav-justified">
       <li>
-        <a href="{{ site.baseurl }}/agricultural" role="button"><span class="icon icon-farm"></span><br>Agricultural</a>
+        <a href="/category/agricultural" role="button"><span class="icon icon-farm"></span><br>Agricultural</a>
       </li>
       <li>
-        <a href="{{ site.baseurl }}/commercial" role="button"><span class="icon icon-commercial"></span><br>Commercial</a>
+        <a href="/category/commercial" role="button"><span class="icon icon-commercial"></span><br>Commercial</a>
       </li>
       <li>
-        <a href="{{ site.baseurl }}/equestrian" role="button"><span class="icon icon-horse"></span><br>Equestrian</a>
+        <a href="/category/equestrian" role="button"><span class="icon icon-horse"></span><br>Equestrian</a>
       </li>
       <li>
-        <a href="{{ site.baseurl }}/residential" role="button"><span class="icon icon-residential"></span><br>Residential</a>
+        <a href="/category/residential" role="button"><span class="icon icon-residential"></span><br>Residential</a>
       </li>
       <li>
-        <a href="{{ site.baseurl }}/storage" role="button"><span class="icon icon-storage"></span><br>Storage</a>
+        <a href="/category/storage" role="button"><span class="icon icon-storage"></span><br>Storage</a>
       </li>
     </ul>
   </nav>
